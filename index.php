377b9b5166d110466f9e4460a7f7dc603465e0ae
Faiza Farooq <faiza.farooq@ssuet.edu.pk>
2022-11-28 12:54:13 -0800

    First Commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<form action="page1.php" method="get">
+<table width="200" border="1">
+  <tr>
+    <th colspan="3" scope="col">LOGIN</th>
+    </tr>
+  <tr>
+    <td>Username</td>
+    <td>&nbsp;</td>
+    <td><label for="uname"></label>
+      <input type="text" name="uname" id="uname" /></td>
+  </tr>
+  <tr>
+    <td>Password</td>
+    <td>&nbsp;</td>
+    <td><label for="password"></label>
+      <input type="password" name="password" id="password" /></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td><input type="submit" name="SUBMIT" id="SUBMIT" value="Submit" /></td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+</form>
+</body>
+</html>
---
+++ page1.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style>
+body {
+	background-image: url(Zz0zZTliMjQ4MzhlNGExMWViYmJiMjFiZTI2ZWNmN2MzZA==.jpg);
+	background-repeat: repeat;
+	background-color: #FFF;
+}
+img {
+            float:right;
+        }
+</style>
+</head>
+<body>
+<h1> "WELCOME TO MY PAGE" </h1>
+<h2> YOUR NAME </h2>
+<h3>About Me</h3>
+<img style=" padding-left:350px;" src="download.png" width="219" height="230" alt="profilepicture" />
+<p> My name is XYZ, I am a student of ABC university.<br /> 
+I am a front-end developer.</p>
+<h3> Capabilities</h3>
+
+<ul>
+  <li>Web Design</li>
+  <li>UI/UX Design</li>
+  <li>Front-end developer</li>
+</ul>
+<h3>Programming Languages and Operating Systems</h3>
+
+<ul>
+  <li>C language</li>
+  <li>C++ </li>
+  <li>Java</li>
+  <li>Python</li>
+  <li>Linux </li>
+</ul>
+</div>
+</body>
+</html>
